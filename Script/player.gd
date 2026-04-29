5287888e60fcf0c7f82bd2c96143202e6232743d
Yanno <128984732+Im-Yannooo@users.noreply.github.com>
2026-04-29 21:30:44 +0800

    Revert "FlashLight System Added"

--- Script/player.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-
-@export var speed = 200
-
-func _physics_process(delta):
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = input_dir * speed
-	move_and_slide()
