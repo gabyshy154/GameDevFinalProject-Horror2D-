4ffa76298fce2550e3f69848cb6906aca6b3dfa9
Yanno <128984732+Im-Yannooo@users.noreply.github.com>
2026-04-29 22:23:47 +0800

    flash mechanic work and sprite implemented

--- Script/player.gd
+++ Script/player.gd
@@ -2,7 +2,23 @@ extends CharacterBody2D
 
 @export var speed = 200
 
-func _physics_process(delta):
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+func _physics_process(_delta):
+	var input_dir = Input.get_vector("left", "right", "up", "down")
 	velocity = input_dir * speed
 	move_and_slide()
+
+	if input_dir.x != 0:
+		$AnimatedSprite2D.flip_h = input_dir.x < 0
+
+	_animation(input_dir)
+
+func _animation(dir):
+	
+	if dir != Vector2.ZERO:
+		$AnimatedSprite2D.play("Walking")
+	else:
+		$AnimatedSprite2D.play("Idle")
+
+func _input(event):
+	if event.is_action_pressed("flashlight"):
+		$PointLight2D.enabled  = !$PointLight2D.enabled 
